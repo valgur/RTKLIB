33570d4c0be1414025ac40796af170391f642ec3
Tim Everett <tim.everett3@gmail.com>
2021-01-03 17:20:35 -0700

    Update code version to b33f

--- src/rtklib.h
+++ src/rtklib.h
@@ -58,7 +58,7 @@ extern "C" {
 
 #define VER_RTKLIB  "demo5"             /* library version */
 
-#define PATCH_LEVEL "b33e"               /* patch level */
+#define PATCH_LEVEL "b33f"               /* patch level */
 
 #define COPYRIGHT_RTKLIB \
             "Copyright (C) 2007-2019 T.Takasu\nAll rights reserved."
